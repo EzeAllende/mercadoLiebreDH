de015ddcdc76b88d2b91f2351ac0f73faeeea30d
EzeAllende <ezequielallende@gmail.com>
2023-05-26 21:42:17 -0300

    Subiendo primera parte del Html

--- app.js
+++ app.js
@@ -2,4 +2,12 @@ const express = require("express");
 const app = express ();
 
 const path = require ("path");
+const publicPath = path.join(__dirname, './public');
 
+app.use (express.static (publicPath));
+
+app.listen (3030, () => {console.log("Servidor Corriendo")});
+
+app.get ('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+});
